e62c5737ca9a8e736d46bf799b40fd4cb9330af2
ljubomir-matovic <ljubomir.matovic@quantoxtechnology.com>
2021-09-21 11:05:45 +0200

    Added HTML, CSS and JS files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let parent = null;
+let clickable = document.getElementsByClassName();
+const showElement = (e) => {
+
+};
+clickable.forEach(element => {
+    element.addEventListener("click",showElement);
+});
