c3ef7ceeed9bfbc6b2f1d3cb62545f3570338365
ljubomir-matovic <ljubomir.matovic@quantoxtechnology.com>
2021-09-22 10:25:59 +0200

    onclick event added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-let parent = null;
-let clickable = document.getElementsByClassName();
+let clickable = document.getElementsByClassName("question");
 const showElement = (e) => {
-
+    let parent = e.target.parentElement.parentElement;
+    let classLists = [parent.querySelector("img.question").classList,parent.querySelector(".answer").classList,parent.querySelector("p.question").classList];
+    classLists.forEach((c) => {
+        if (c.contains("active"))
+            c.remove("active");
+        else
+            c.add("active");
+    });
 };
-clickable.forEach(element => {
+[...clickable].forEach(element => {
     element.addEventListener("click",showElement);
 });
