af4763da0ff072522dd69771395a20e810f497e0
ljubomir-matovic <ljubomir.matovic@quantoxtechnology.com>
2021-09-22 13:15:30 +0200

    Responsive finished

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 let clickable = document.getElementsByClassName("question");
 const showElement = (e) => {
-    let parent = e.target.parentElement.parentElement;
-    let classLists = [parent.querySelector("img.question").classList,parent.querySelector(".answer").classList,parent.querySelector("p.question").classList];
-    classLists.forEach((c) => {
-        if (c.contains("active"))
-            c.remove("active");
-        else
-            c.add("active");
-    });
+    if (!e.target.classList.contains("active")) {
+        let activeQuestions = document.querySelectorAll(".active");
+        [...activeQuestions].forEach((element) => {
+            element.classList.remove("active");
+        });
+    }
+        let parent = e.target.parentElement.parentElement;
+        let classLists = [parent.querySelector("img.question").classList, parent.querySelector(".answer").classList, parent.querySelector("p.question").classList];
+        classLists.forEach((c) => {
+            if (c.contains("active"))
+                c.remove("active");
+            else
+                c.add("active");
+        });
 };
 [...clickable].forEach(element => {
-    element.addEventListener("click",showElement);
+    element.addEventListener("click", showElement);
 });
